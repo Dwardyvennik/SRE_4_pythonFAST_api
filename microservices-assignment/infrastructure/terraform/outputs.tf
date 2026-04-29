66f85c18f626bbb3200509d1bd06f87d001891e6
Dwardyvennik <167704504+Dwardyvennik@users.noreply.github.com>
2026-04-29 22:26:22 +0500

    Delete microservices-assignment directory

--- microservices-assignment/infrastructure/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_name" {
-  description = "Configured project name."
-  value       = local.project_name
-}
-
-output "environment" {
-  description = "Configured environment name."
-  value       = local.environment
-}
